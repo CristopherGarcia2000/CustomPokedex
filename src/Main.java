8bcd168a21f26749d16e6791e201b70010cfd1ed
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2023-04-18 10:51:00 +0100

    feat: Creado el menu, para añadir pokemon normales y legendarios y pasarlos de avistados a capturados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
+import net.salesianos.pokemitos.general.Pokemon;
+import net.salesianos.pokemitos.legendarios.Legendario;
+import net.salesianos.utilidades.ListaPokemon;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int opcion = -1;
+        String nombrePokemon,tipo,localizacion;
+        ListaPokemon avistados = new ListaPokemon();
+        ListaPokemon capturados = new ListaPokemon();
+        Scanner teclao = new Scanner(System.in);
+        while (opcion != 6){
+            System.out.println("Elija la opcion que desea: ");
+            System.out.println("1.-Añadir pokemon avistado");
+            System.out.println("2.-Añadir pokemon legendario avistado");
+            System.out.println("3.-Pasar a Capturados");
+            System.out.println("4.-Mostrar lista de avistados");
+            System.out.println("5.-Mostrar lista de capturados");
+            System.out.println("6.-Cerrar Pokedex");
+            opcion = teclao.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.println("Introduzca el nombre del pokemon legendario:");
+                    nombrePokemon = teclao.nextLine();
+                    System.out.println("Introduzca el tipo del pokemon legendario:");
+                    tipo = teclao.nextLine();
+                    Pokemon pokemonito = new Pokemon(nombrePokemon,tipo);
+                    avistados.addPokemon(pokemonito);
+                    break;
+                case 2:
+                    System.out.println("Introduzca el nombre del pokemon:");
+                    nombrePokemon = teclao.nextLine();
+                    System.out.println("Introduzca el tipo del pokemon:");
+                    tipo = teclao.nextLine();
+                    System.out.println("Introduzca la localizacion del pokemon:");
+                    localizacion = teclao.nextLine();
+                    Legendario pokemonLegendario = new Legendario(nombrePokemon,tipo,localizacion);
+                    avistados.addPokemon(pokemonLegendario);
+                    break;
+                case 3:
+                    System.out.println("Introduzca el nombre del pokemon que haya capturado");
+                    nombrePokemon = teclao.nextLine();
+                    if(avistados.devolverPokemon(nombrePokemon) != null){
+                        capturados.addPokemon(avistados.devolverPokemon(nombrePokemon));
+                        avistados.deletePokemon(nombrePokemon);
+                    }else {
+                        System.out.println(nombrePokemon + " no está");
+                    }
+
+                    break;
+                case 4:
+                    avistados.mostrarPokemon();
+                    break;
+                case 5:
+                    capturados.mostrarPokemon();
+                    break;
+                case 6:
+                    System.out.println("Hasta la próxima");
+                    break;
+                default:
+                    System.out.println("Mostro, un número válido");
+            }
+        }
+
     }
 }
--- src/net/salesianos/utilidades/ListaPokemon.java
+++ src/net/salesianos/utilidades/ListaPokemon.java
@@ -4,63 +4,67 @@ import net.salesianos.pokemitos.general.Pokemon;
 
 public class ListaPokemon {
     private int contador = 0;
-    private Pokemon listaPokemon [] = new Pokemon[1];
+    private Pokemon listaPokemon[] = new Pokemon[1];
 
-    public ListaPokemon(Pokemon[] listaPokemon) {
-        this.listaPokemon = listaPokemon;
+    public ListaPokemon() {
+
     }
 
 
-    public void addPokemon(Pokemon pokemon){
+    public void addPokemon(Pokemon pokemon) {
         listaPokemon[contador] = pokemon;
         contador++;
 
         Pokemon listaTemporal[] = new Pokemon[contador + 1];
-        for (int i = 0; i < listaPokemon.length; i ++) {
-            listaTemporal[i]=listaPokemon[i];
+        for (int i = 0; i < listaPokemon.length; i++) {
+            listaTemporal[i] = listaPokemon[i];
         }
         this.listaPokemon = listaTemporal;
 
     }
-    public void deletePokemon(String pokeNombre){
+
+    public void deletePokemon(String pokeNombre) {
         int size = 0;
-        for (int i = 0; i < listaPokemon.length-1; i++) {
-            if(listaPokemon[i].getNombre().equals(pokeNombre)){
+        for (int i = 0; i < listaPokemon.length - 1; i++) {
+            if (listaPokemon[i].getNombre().equals(pokeNombre)) {
                 listaPokemon[i] = null;
                 System.out.println("El pokemon ha sido borrado");
-            }else {
+            } else {
                 if (i == listaPokemon.length) {
                     System.out.println("El pokemon no está registrado33");
                 }
                 size++;
             }
         }
-        Pokemon temporalList[] = new Pokemon[contador-1];
+        Pokemon temporalList[] = new Pokemon[contador - 1];
         int includedPokemons = 0;
-        for (int i = 0; i < listaPokemon.length-1; i++) {
+        for (int i = 0; i < listaPokemon.length - 1; i++) {
 
-            if(listaPokemon[i] != null){
-                temporalList[includedPokemons]=listaPokemon[i];
+            if (listaPokemon[i] != null) {
+                temporalList[includedPokemons] = listaPokemon[i];
             }
         }
         this.listaPokemon = temporalList;
     }
-    public void mostrarPokemon(){
+
+    public void mostrarPokemon() {
         String msg = "";
         for (int i = 0; i < listaPokemon.length; i++) {
-            if(listaPokemon[i]!=null){
-                msg += '\n'+listaPokemon[i].toString();
+            if (listaPokemon[i] != null) {
+                msg += '\n' + listaPokemon[i].toString();
             }
         }
         System.out.println(msg);
     }
-    public Pokemon devolverPokemon(int posicion){
-        if(listaPokemon[posicion] == null){
-            System.out.println("No hay pokemon donde se ha indicado");
-            return null;
-        }else{
-            return listaPokemon[posicion];
-        }
-    }
 
+    public Pokemon devolverPokemon(String nombrePokemon) {
+        for (int i = 0; i < listaPokemon.length; i++) {
+            if (listaPokemon[i].getNombre().equals(nombrePokemon)) {
+                return listaPokemon[i];
+            }
+        }
+        return null;
+    }
 }
+
+
